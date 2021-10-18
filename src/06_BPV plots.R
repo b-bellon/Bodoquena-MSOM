0d021c7755a0de321db768692ff0039212eb4a75
Dominic Henry <dominichenry@gmail.com>
2021-10-18 13:58:18 -0400

    Code to produce simple BPV plots

---
+++ src/06_BPV plots.R
@@ -0,0 +1,39 @@
+## Libraries
+library(tidyverse)
+library(boot)
+library(coda)
+library(nimble)
+library(glue)
+
+## ________________________________________________________________________
+
+# Define spatial scale ----------------------------------------------------
+spatscale <- 250 # meters
+
+# Read in data ------------------------------------------------------------
+spatdir <- glue("data output/modelout_{spatscale}m")
+load(glue("data output/model_data_{spatscale}m.RData"))
+
+runMCMC_samples <- readRDS(glue("{spatdir}/mammal_mcmc_samples_{spatscale}m_BPV.rds"))
+mod <- as.matrix(runMCMC_samples$samples)
+dim(mod)
+
+att <- attributes(mod)$dimnames[2][[1]][]
+att
+
+# Extract BPV and plots ---------------------------------------------------
+bpv_posterior <- mod[,"test"]
+round(mean(bpv_posterior),2)
+table(bpv_posterior)
+
+par(mfrow = c(2,2))
+
+dev <- mod[,"sum.dev"]
+hist(dev)
+
+dev.sim <- mod[,"sum.dev.sim"]
+hist(dev.sim)
+
+plot(dev,dev.sim, main = glue("Deviance at {spatscale}m scale"), xlim = c(7000,10000),  ylim = c(7000,10000))
+abline(0,1, col = "red", lwd = 2)
+
